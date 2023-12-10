d8227e7913280ec64a0d638548c6b50abd1932a4
khalilarfaoui <khalilwin1920@gmail.com>
2023-12-10 20:16:38 +0100

    Initial commit

---
+++ src/main/java/com/biblio/bnr/BnrApplication.java
@@ -0,0 +1,13 @@
+package com.biblio.bnr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biblio/bnr/BnrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblio.bnr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
